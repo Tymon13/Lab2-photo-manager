fc17b8f2f5c2000d0b8c6f140d59b3fc5bf76a13
Kamil Tymiński <kamil_tym@wp.pl>
2021-03-11 22:15:27 +0100

    Add FAB to main activity
    Back to the Past

--- app/src/main/java/pl/edu/pwr/lab2/i238162/MainActivity.java
+++ app/src/main/java/pl/edu/pwr/lab2/i238162/MainActivity.java
@@ -1,8 +1,10 @@
 package pl.edu.pwr.lab2.i238162;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onFabClick(View v) {
+        Toast.makeText(this, "Not implemented, heh", Toast.LENGTH_LONG).show();
     }
 }
