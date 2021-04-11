3812b9cf05b6ad7bd8d68149d7bd13a81731b48d
Kamil Tymiński <kamil_tym@wp.pl>
2021-04-12 00:48:48 +0200

    Add video details activity
    You can finally watch the video after saving. That's good, no?

--- app/src/main/java/pl/edu/pwr/lab2/i238162/VideoDetails.java
+++ app/src/main/java/pl/edu/pwr/lab2/i238162/VideoDetails.java
@@ -1,14 +1,37 @@
 package pl.edu.pwr.lab2.i238162;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ui.StyledPlayerView;
+
+import java.io.File;
+
 public class VideoDetails extends AppCompatActivity {
+    private SimpleExoPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_details);
+
+        String filePath = getIntent().getStringExtra("filePath");
+
+        player = new SimpleExoPlayer.Builder(this).build();
+        StyledPlayerView playerView = findViewById(R.id.player_view);
+        playerView.setPlayer(player);
+        MediaItem video = MediaItem.fromUri(Uri.fromFile(new File(filePath)));
+        player.addMediaItem(video);
+        player.prepare();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        player.release();
     }
 }
